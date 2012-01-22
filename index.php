9c39390022b19e1aef56c4dfbbb111df27f7cffe
Josh Adell <josh.adell@gmail.com>
2012-01-22 13:59:06 -0500

    Test connection to Neo4j

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-echo "Hello, world.";
+use Everyman\Neo4j\Client;
+
+require('phar://neo4jphp.phar');
+
+$client = new Client('692740c56.hosted.neo4j.org', 7048);
+$client->getTransport()->setAuth('4f0d4c9c2', 'ccb7b7898');
+print_r($client->getServerInfo());
