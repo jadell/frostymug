1aa2765d50fde8c50f1c7c88c3b8443b4d35a27a
Josh Adell <josh.adell@gmail.com>
2012-01-22 14:31:02 -0500

    Check environment var for url

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ use Everyman\Neo4j\Client;
 
 require('phar://neo4jphp.phar');
 
+echo getenv('NEO4J_REST_URL');
+
 $client = new Client('692740c56.hosted.neo4j.org', 7048);
 $client->getTransport()->setAuth('4f0d4c9c2', 'ccb7b7898');
 
